ebc4bcbb6eb579ba3754d2f8b4048b077b3c4126
Hatonwood <Scar11969@hotmail.com>
2016-07-06 15:34:07 +0100

    Init commit

---
+++ Sundit/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Sundit
+{
+    [Activity(Label = "Sundit", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            //Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            //Profile pic query
+            ImageButton profilepic = FindViewById<ImageButton>(Resource.Id.profilepic);
+
+            //username query
+            TextView username = FindViewById<TextView>(Resource.Id.username);
+            username.Text = "Username";
+
+            //properties image
+            ImageButton properties = FindViewById<ImageButton>(Resource.Id.properties);
+            //properties.SetImageResource;
+
+            //refresh Timeline
+            //query to the server
+        }
+    }
+}
+
